898a7261e8098be076dc763ba3e57376b67acd48
danhkhai07 <aliveomg@gmail.com>
2025-05-12 21:32:33 +0700

    try fmt

--- 01H/main.cpp
+++ 01H/main.cpp
@@ -1,6 +1,8 @@
-#include "fmt/color.h"
+#include <fmt/color.h>
+#include <iostream>
 
 int main(){
-    
+    // fmt::print(fg(fmt::color::steel_blue) | fmt::emphasis::italic, "你好{}！\n", "世界");
+    std::cin.get();
     return 0;
 }
